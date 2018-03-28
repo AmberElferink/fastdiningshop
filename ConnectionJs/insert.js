8dde18451acd83f37d5ff5eda6e95bc821695e0d
Amber Elferink <a.s.elferink@students.uu.nl>
2018-03-28 17:36:50 +0200

    Hij console.logt nu alle persons info als je in de ConnectionJS map node insert.js aanroept.

--- ConnectionJs/insert.js
+++ ConnectionJs/insert.js
@@ -1,12 +1,24 @@
 var sqlite3 = require('sqlite3').verbose();
 
 //creates a new database
-let db = new sqlite3.Database(':memory:', (err) => {
+let db = new sqlite3.Database('fastdining.db', (err) => {
     if (err) {
         return console.error(err.message);
     }
     console.log('Connected to the in-memory SQlite database');
     });
+
+
+db.all("SELECT * FROM Persons", [], function(err, rows){
+    console.log(rows);
+});
+
+
+//db.get als je een tupel wil krijgen
+//geeft het terug in een javascript object
+//row.name geeft naam
+//db.all als je alle tupels wil in een array. Elk object is een element uit een array
+
 
 //wacht tot alle queries klaar zijn en sluit de database dan af
 db.close((err) => {
@@ -15,3 +27,4 @@ db.close((err) => {
     }
     console.log('Close the database connection.');
 });
+

6169456f0f809b78a5d05a2f8898f41816f8b060
Amber Elferink <a.s.elferink@students.uu.nl>
2018-04-03 14:14:46 +0200

    productDescription wordt als HTML weergegeven

--- routes/productDescription.js
+++ routes/productDescription.js
@@ -6,9 +6,9 @@ var router = express.Router();
 
 //
 router.get('/:barcode', function(req, res, next) {
-    console.log(req.params.barcode); //geeft de barcode die je kan gebruiken voor inladen van je zooi
-    res.render('product', {title: 'Buy Products', layout: 'productDescriptionLayout', description: 'ik ben een banaan'});
+    res.render('product', {title: 'Buy Products', layout: 'productDescriptionLayout'});
 });
+
 
 
 module.exports = router;

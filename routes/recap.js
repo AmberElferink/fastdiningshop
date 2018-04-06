f3345f9b6981a29bd34fdb3824e8fa5fbdf25ab0
anneloesmeijer <a.l.meijer2@students.uu.nl>
2018-04-06 15:04:08 +0200

    Login validation werkt met array, registerform is er maar werkt nog niet

--- routes/recap.js
+++ routes/recap.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('products', { title: 'Login' });
+    res.render('recap', { title: 'Recap' , layout: 'layoutRecap'});
 });
 
 module.exports = router;

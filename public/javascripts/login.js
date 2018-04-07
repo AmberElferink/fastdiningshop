0f0a28a5f39886f43adfc82f8721ca66d7234d39
Amber Elferink <a.s.elferink@students.uu.nl>
2018-04-07 23:58:43 +0200

    returnt false bij verkeerde login uit database

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -21,19 +21,55 @@
 }
 
 */
+
+
+
+
 function validate() {
-    var un = document.login.username.value;
-    var pw = document.login.password.value;
+    var un = $('#username').val();
+    var pw = $('#password').val();
     var valid = false;
-    var usernameArray = ["Vlad", "bob"];
+
+    postLogin(function (returnValue) {
+        console.log(returnValue);
+        //giveAlert(returnValue);
+    }, un, pw);
+
+    /*var usernameArray = ["Vlad", "bob"];
     var passwordArray = ["12345", "hallo"];
     for (var i = 0; i < usernameArray.length; i++) {
         if ((un == usernameArray[i]) && (pw == passwordArray[i])) {
             valid = true;
             break;
         }
-    }
-    if (valid) {
+    }*/
+
+}
+
+function postLogin(callback, un, pw) {
+    console.log(un,pw)
+    $.ajax({
+        type: 'POST',
+        url: '/api/checkLogin',
+        dataType: 'json',
+        data: {
+            "username": un,
+            "password": pw}
+    })//als deze asynchronous ajax call klaar is, is het of gefaald, of goed gegaan.
+    //als het goed is gegaan, callt hij de .done hieronder.
+        .done(function (data) {
+            //deze done functie logt het naar de javascript console en print het op de pagina als txt
+            //console.log('GET response:', JSON.stringify(data, "", 2));
+            callback(data);
+        })
+        //als het niet goed is gegaan, doet hij de fail hieronder
+        .fail(function (jqXHR, textStatus, err) {
+            console.log('AJAX error response:', textStatus);
+        });
+}
+
+function giveAlert(returnValue) {
+    if (returnValue == true) {
         alert("Login was successful");
     }
 }

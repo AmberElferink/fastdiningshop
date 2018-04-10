e87b628a0f90819df720d6a41c700b35c8765473
Amber Elferink <a.s.elferink@students.uu.nl>
2018-04-10 14:29:23 +0200

    betere doorverwijzing gemaakt na register

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -16,10 +16,8 @@
         //als het goed is gegaan, callt hij de .done hieronder.
                 .done(function (data) {
                     console.log(data);
-                    //logs if username already existed (false is good)
-                    console.log('GET response:', JSON.stringify(data, "", 2));
-                    $('#getResponse').html(JSON.stringify(data, "", 2));
-                    alert("register succesful");
+                    alert("register succesful, please log in to continue");
+                    window.location.replace("/login");
                 })
                 //als het niet goed is gegaan, doet hij de fail hieronder
                 .fail(function (jqXHR, textStatus, err) {

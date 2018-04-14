ad7a6aafd682b6f030db8e698964628f89130c67
Amber Elferink <a.s.elferink@students.uu.nl>
2018-04-14 12:44:20 +0200

    geeft weer welke pagina hij vandaan kwam op /loginValid

--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -20,20 +20,25 @@ $('#logout').click(function () {
 
 console.log("navbar javascript loaded");
 $('#historybutton').click(function () {
+    let path = window.location.pathname;
+    let path2 = '?path2='+path.substr(1);
+    let search = window.location.search;
+    if(search)
+    {
+        search = '&search2=' + window.location.search;
+    }
+    else
+    {
+        search = "";
+    }
     $.ajax({
         type: 'GET',
-        url: './loginValidate',
+        url: './loginValidate' + path2 + search,
         dataType: 'text',
     })//als deze asynchronous ajax call klaar is, is het of gefaald, of goed gegaan.
     //als het goed is gegaan, callt hij de .done hieronder.
         .done(function (data) {
-
-            console.log(data);
-
-            //deze done functie logt het naar de javascript console en print het op de pagina als txt
-            //console.log('GET response:', JSON.stringify(data, "", 2));
-            //window.location.replace("./");
-            alert(data); //gives "logout was succesful" if user was logged in, otherwise nothing.
+            window.location.assign('./loginValidate' + path2 + search);
         })
         //als het niet goed is gegaan, doet hij de fail hieronder
         .fail(function (jqXHR, textStatus, err) {
--- routes/loginValidate.js
+++ routes/loginValidate.js
@@ -12,12 +12,28 @@ session = require('express-session');
 
 router.get('/',function(req, res, next) {
     if(req.session) {
-        res.send(req.session.user);
+        res.send(req.session.user + " origin: " + req.query.path2 + " "+ req.query.search2);
         //req.session.errors = null;
     } else{
         res.send("No User");
     }
 });
+
+
+//code from: https://medium.com/@nikjohn/express-js-node-js-extract-path-from-request-object-529ceef2c7e5
+function pathExtractor(req) {
+    // Escaping user input to be treated as a literal
+    // string within a regular expression accomplished by
+    // simple replacement
+    function escapeRegExp(str) {
+        return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, '\\$1');
+    }
+    // Replace utility function
+    function replaceAll(str, find, replace) {
+        return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+    }
+    return replaceAll(req.get('referer'), req.get('origin'), '');
+}
 
 
 module.exports = router;

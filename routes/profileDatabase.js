95404e7769243555a6672fc15b1dfaf75f20409b
Amber Elferink <a.s.elferink@students.uu.nl>
2018-04-14 21:50:54 +0200

    update werkt

--- routes/profileDatabase.js
+++ routes/profileDatabase.js
@@ -1,10 +1,8 @@
 
-
 var sqlite3 = require('sqlite3').verbose();
 var fs = require("fs");
 var file = __dirname + "/../ConnectionJs/fastdining.db";
 var exists = fs.existsSync(file);
-
 var express = require('express');
 var router = express.Router();
 
@@ -30,44 +28,7 @@ router.post('/', function (req, res) {
     },req.body);
 });
 
-router.get('/', function (req, res) {
-    console.log(req.query);
-    selectProfileFromDatabase(function(err, returnValues){
-    res.send(returnValues);
-    }, req.query.username);
-});
-
 module.exports = router;
-
-
-function selectProfileFromDatabase(callback, profileUser) {
-    let db = new sqlite3.Database(file, (err) => {
-        if (err) {
-            return console.error(err.message);
-        }
-        console.log('Connected to the database');
-
-    });
-    db.serialize(function () {
-        console.log(profileUser);
-        db.all("SELECT * FROM Persons WHERE username=?", [profileUser], function (err, rows) {
-            console.log(rows);
-            if (err) {
-                return callback(err);
-            }
-            //de eerste moet je op undefined zetten, omdat hij anders in de aanroep de rows als error teruggeeft,
-            //en dan zijn de returnValues dus undefined
-            callback(undefined, rows);
-        });
-    //wacht tot alle queries klaar zijn en sluit de database dan af
-    db.close((err) => {
-        if (err) {
-            return console.error(err.message);
-        }
-        console.log('Close the database connection.');
-    });
-});
-}
 
 
 
@@ -85,73 +46,50 @@ function addNewProfileToDatabase(callback, newProfileData) {
 
     //selects correct tuple and checks if password is the same as the entered password.
     db.serialize(function () {
-        var newUsernameValid = true;
-        db.each("SELECT personid FROM Persons WHERE username=?", [newProfileData.username], function (err, row) {
-            console.log(row);
+        console.log(newProfileData)
+        // insert one row into the langs table
+        db.run("UPDATE Persons SET firstname=?, surname=?, username=?, password=?, emailaddress=? WHERE username=?", [newProfileData.firstname, newProfileData.surname, newProfileData.username, newProfileData.password, newProfileData.emailaddress, newProfileData.oldUserName], function (err) {
+            if (err) {
+                return console.log(err.message);
+            }
+            // get the last insert id
+            console.log(`A row has been inserted with rowid ${this.lastID}`);
+            console.log(newProfileData);
+        });
+    });
+
+    db.all("SELECT * FROM Persons", function (err, row) {
+        console.log(row);
+        for(var i = 0; i < row.length; i++) {
             if (err) {
                 return callback(err);
             }
-            if (row != undefined) {
-                console.log("new username is in database");
-                newUsernameValid = false;
-                callback(undefined, false);
+            if (row == undefined) {
                 //username not found in database
-                return;
-            }
-        });
-        if (newUsernameValid) {
-            db.each("SELECT personid FROM Persons WHERE username=?", [oldUserName], function (err, row) {
-                console.log(row);
-            });
-            db.run(`DELETE FROM Persons WHERE username=?`, [newProfileData.oldUserName], function (err) {
-                if (err) {
-                    return console.error(err.message);
-                }
-            });
-            // insert one row into the langs table
-            db.run(`INSERT INTO Persons(personid, firstname, surname, username, password, emailaddress) VALUES(?, ?, ?, ?, ?)`, [personid, newProfileData.firstname, newProfileData.surname, newProfileData.username, newProfileData.password, newProfileData.emailaddress], function (err) {
-                if (err) {
-                    return console.log(err.message);
-                }
-                // get the last insert id
-                console.log(`A row has been inserted with rowid ${this.lastID}`);
-                console.log(newProfileData);
-            });
-
-            db.all("SELECT * FROM Persons", function (err, row) {
-                console.log(row);
-                for (var i = 0; i < row.length; i++) {
-                    if (err) {
-                        return callback(err);
-                    }
-                    if (row == undefined) {
-                        //username not found in database
-                        callback(undefined, false);
-                        return;
-                    }
-                    else if (row[i].password == newProfileData.password) {
-                        callback(undefined, true);
-                        return;
-                    }
-                    else {
-                        //password not correct
-                        callback(undefined, false);
-                        return;
-                    }
-                }
-                //username not found in database row.length = 0
                 callback(undefined, false);
                 return;
-            })
-        }
-
-
-        //wacht tot alle queries klaar zijn en sluit de database dan af
-        db.close((err) => {
-            if (err) {
-                return console.error(err.message);
             }
-            console.log('Close the database connection.');
-        });
-    })
+            else if (row[i].password == newProfileData.password) {
+                callback(undefined, true);
+                return;
+            }
+            else {
+                //password not correct
+                callback(undefined, false);
+                return;
+            }
+        }
+        //username not found in database row.length = 0
+        callback(undefined, false);
+        return;
+    });
+
+    //wacht tot alle queries klaar zijn en sluit de database dan af
+    db.close((err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log('Close the database connection.');
+    });
 }
+module.exports = router;
